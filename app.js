9240ac482855f1f2140f1e0a4d2a3ad8987628d6
Will Schumacher <meintodengel@gmail.com>
2016-03-25 11:57:27 -0700

    test

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const database = new Firebase('https://pingpongratings.firebaseio.com');
-
-$('form').on('submit', () => {
-  const winner = $('#winner').val();
-  const loser = $('#loser').val();
-
-  database.push({winner: winner, loser: loser});
-});
+// const database = new Firebase('https://pingpongratings.firebaseio.com');
+//
+// $('form').on('submit', () => {
+//   const winner = $('#winner').val();
+//   const loser = $('#loser').val();
+//
+//   database.push({winner: winner, loser: loser});
+// });
 
 // function expOutcome(ratingA, ratingB) {
 //   return 1 / (1 + Math.pow(10, (ratingA - ratingB) / 400))
