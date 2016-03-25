30f2bd0e95ad40094bea206bbbb5cd569ec12c40
Will Schumacher <meintodengel@gmail.com>
2016-03-25 14:11:20 -0700

    set static filepath for express

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
+const bp = require('body-parser');
 const port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 const ipAdress = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'))
+app.use(bp.json());
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.post('/post/', (req, res) => {
+  console.log('hello');
+  console.log(req.body);
+  res.send('hello');
+})
 
 app.listen(port, ipAdress, () => {
   console.log(`listening on ${ipAdress}, port ${port}`);
