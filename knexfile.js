0c52ca009a07ea0724e388e7c1e8c23545b3a5a8
Will Schumacher <meintodengel@gmail.com>
2016-03-30 12:00:42 -0700

    add knex migrations to create db

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/ping-pong-rating'
+    connection: 'postgres://localhost/ping-pong-ratings'
   },
 
   production: {
